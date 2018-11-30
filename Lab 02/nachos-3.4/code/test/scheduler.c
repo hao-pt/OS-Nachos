68a8619cbaba1d07852f8304c9ff3a0fe6d77178
tienhaophung <tienhaophung@gmail.com>
2018-11-30 20:29:33 +0700

    Post source do an 2

---
+++ Lab 02/nachos-3.4/code/test/scheduler.c
@@ -0,0 +1,13 @@
+#include "syscall.h"
+
+void main()
+{
+	int pingPID, pongPID;
+	PrintString("Ping-Pong test starting...\n\n");
+	pingPID = Exec("./test/ping");
+	pongPID = Exec("./test/pong");
+	Join(pingPID);
+	Join(pongPID);
+	PrintString("\n\n");
+}
+
